16967c148bdc82258f9613213a8c1b01e7ee697b
Ram_Prakash <102049599+ram75981@users.noreply.github.com>
2023-05-11 15:33:34 +0530

    Add files via upload

---
+++ RamPrakash-whatsapp/js/json.js
@@ -0,0 +1,92 @@
+var contactlist = [
+    {
+        "userid" : "Uid1",
+        "image" : "img/pp.jpg",
+        "name" : "Dinesh",
+        "time" : "7.55 PM",
+        "tick" : "fa-check",
+        "message" : "Lorem ipsum, dolor sit amet consectetur",
+        "mute" : "fa-volume-xmark",
+        "unread" : ""
+    },
+    {
+        "userid" : "Uid2",
+        "image" : "img/pp_1.jpg",
+        "name" : "Siva",
+        "time" : "7.55 PM",
+        "tick" : "fa-check",
+        "message" : "Lorem ipsum, dolor sit amet consectetur",
+        "mute" : "fa-volume-xmark",
+        "unread" : ""
+    },
+    {
+        "userid" : "Uid3",
+        "image" : "img/pp_2.jpg",
+        "name" : "Gokul",
+        "time" : "7.55 PM",
+        "tick" : "fa-check",
+        "message" : "Lorem ipsum, dolor sit amet consectetur",
+        "mute" : "fa-volume-xmark",
+        "unread" : ""
+    },
+    {
+        "userid" : "Uid4",
+        "image" : "img/pp_3.jpg",
+        "name" : "Venkatesh",
+        "time" : "7.55 PM",
+        "tick" : "fa-check",
+        "message" : "Lorem ipsum, dolor sit amet consectetur",
+        "mute" : "fa-volume-xmark",
+        "unread" : ""
+    },
+    {
+        "userid" : "Uid5",
+        "image" : "img/pp_4.jpg",
+        "name" : "hari",
+        "time" : "7.55 PM",
+        "tick" : "fa-check",
+        "message" : "Lorem ipsum, dolor sit amet consectetur",
+        "mute" : "fa-volume-xmark",
+        "unread" : ""
+    },
+    {
+        "userid" : "Uid6",
+        "image" : "img/pp_5.jpg",
+        "name" : "gow",
+        "time" : "7.55 PM",
+        "tick" : "fa-check",
+        "message" : "Lorem ipsum, dolor sit amet consectetur",
+        "mute" : "fa-volume-xmark",
+        "unread" : ""
+    },
+    {
+        "userid" : "Uid7",
+        "image" : "img/pp_6.jpg",
+        "name" : "ram",
+        "time" : "7.55 PM",
+        "tick" : "fa-check",
+        "message" : "Lorem ipsum, dolor sit amet consectetur",
+        "mute" : "fa-volume-xmark",
+        "unread" : ""
+    },
+    {
+        "userid" : "Uid8",
+        "image" : "img/pp_7.jpg",
+        "name" : "abcd",
+        "time" : "7.55 PM",
+        "tick" : "fa-check",
+        "message" : "Lorem ipsum, dolor sit amet consectetur",
+        "mute" : "fa-volume-xmark",
+        "unread" : ""
+    },
+    {
+        "userid" : "Uid9",
+        "image" : "img/my-pr.jpeg",
+        "name" : "efgh",
+        "time" : "7.55 PM",
+        "tick" : "fa-check",
+        "message" : "Lorem ipsum, dolor sit amet consectetur",
+        "mute" : "fa-volume-xmark",
+        "unread" : ""
+    }
+]
